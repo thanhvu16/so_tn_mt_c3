e3ad83a0c2d8088cb79c2e19eb987d0410b385fa
thanhvu16 <haithanhcx1@gmail.com>
2020-11-24 16:08:23 +0700

    du thao van ban 2

--- Modules/VanBanDi/Http/Controllers/DuThaoVanBanController.php
+++ Modules/VanBanDi/Http/Controllers/DuThaoVanBanController.php
@@ -257,7 +257,15 @@ class DuThaoVanBanController extends Controller
      */
     public function edit($id)
     {
-        return view('vanbandi::edit');
+        $ds_loaiVanBan = LoaiVanBan::whereNull('deleted_at')->whereIn('loai_van_ban', [2, 3])
+            ->orderBy('ten_loai_van_ban', 'desc')->get();
+        $lanhdaotrongphong = User::where(['don_vi_id' => auth::user()->don_vi_id])->whereNull('deleted_at')->get();
+        $lanhdaokhac = User::where(['don_vi_id' => auth::user()->don_vi_id])->whereNull('deleted_at')->get();
+        $ds_nguoiKy = User::orderBy('username', 'desc')->whereNull('deleted_at')->get();
+        $lay_can_bo_phong = CanBoPhongDuThao::where(['du_thao_vb_id' => $id])->whereIn('trang_thai', [1, 2])->get();
+        $lay_can_bo_khac = CanBoPhongDuThaoKhac::where(['du_thao_vb_id' => $id])->whereIn('trang_thai', [1, 2])->get();
+        $duthao = Duthaovanbandi::where('id', $id)->first();
+        return view('vanbandi::Du_thao_van_ban_di.edit', compact('duthao', 'ds_nguoiKy', 'lanhdaokhac', 'lanhdaotrongphong', 'ds_loaiVanBan', 'lay_can_bo_khac', 'lay_can_bo_phong'));
     }
 
     /**
@@ -268,7 +276,271 @@ class DuThaoVanBanController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        {
+            $uploadPath = public_path('vanBanDiFile_' . date('Y'));
+            $tenfilehoso = !empty($request['txt_file']) ? $request['txt_file'] : null;
+            $filehoso = !empty($request['file_name']) ? $request['file_name'] : null;
+            $filephieutrinh = !empty($request['file_phieu_trinh']) ? $request['file_phieu_trinh'] : null;
+            $filetrinhky = !empty($request['file_trinh_ky']) ? $request['file_trinh_ky'] : null;
+            $lanhdaophong = !empty($request['lanh_dao_phong_phoi_hop']) ? $request['lanh_dao_phong_phoi_hop'] : null;
+            $lanhdaophongkhac = !empty($request['lanh_dao_phong_khac']) ? $request['lanh_dao_phong_khac'] : null;
+            $canbophong = CanBoPhongDuThao::where(['du_thao_vb_id' => $id, 'trang_thai' => 1])->get();
+            $idcanbophong = $canbophong->pluck('can_bo_id')->toArray();
+            $canbophongkhac = CanBoPhongDuThaoKhac::where(['du_thao_vb_id' => $id, 'trang_thai' => 1])->get();
+            $idcanbophongkhac = $canbophongkhac->pluck('can_bo_id')->toArray();
+
+            $duthao = Duthaovanbandi::where('id', $id)->first();
+            $duthao->loai_van_ban_id = $request->loai_van_ban_id;
+            $duthao->so_ky_hieu = $request->so_ky_hieu;
+            $duthao->y_kien = $request->y_kien;
+            $duthao->vb_trich_yeu = $request->vb_trich_yeu;
+            $duthao->nguoi_ky = $request->nguoi_ky;
+            $duthao->chuc_vu = $request->chuc_vu;
+            $duthao->ngay_thang = $request->ngay_thang;
+            $duthao->so_trang = $request->so_trang;
+            $duthao->han_xu_ly = $request->han_xu_ly;
+            $duthao->save();
+            if ($filehoso && count($filehoso) > 0) {
+                foreach ($filehoso as $key => $getFile) {
+                    $typeArray = explode('.', $getFile->getClientOriginalName());
+                    $extFile = strtolower($typeArray[1]);
+                    $ten = strSlugFileName(strtolower($tenfilehoso[$key]), '_') . '.' . $extFile;
+                    $fileduthao = new Fileduthao();
+                    $fileName = date('Y_m_d') . '_' . Time() . '_' . $getFile->getClientOriginalName();
+
+                    $urlFile = '/vanBanDiFile_' . date('Y') . '/' . $fileName;
+                    if (!File::exists($uploadPath)) {
+                        File::makeDirectory($uploadPath, 0775, true, true);
+                    }
+                    $getFile->move($uploadPath, $fileName);
+                    $fileduthao->ten_file = $ten;
+                    $fileduthao->duong_dan = $urlFile;
+                    $fileduthao->duoi_file = $extFile;
+                    $fileduthao->vb_du_thao_id = $duthao->id;
+                    $fileduthao->nguoi_tao = $this->user->id;
+                    $fileduthao->don_vi = $this->user->donvi_id;
+                    $fileduthao->stt = 3;
+                    $fileduthao->save();
+                }
+
+            }
+            if ($filephieutrinh && count($filephieutrinh) > 0) {
+                foreach ($filephieutrinh as $key => $getFile) {
+                    $typeArray = explode('.', $getFile->getClientOriginalName());
+                    $extFile = strtolower($typeArray[1]);
+                    $fileduthao = new Fileduthao();
+                    $fileName = date('Y_m_d') . '_' . Time() . '_' . $getFile->getClientOriginalName();
+                    $urlFile = '/vanBanDiFile_' . date('Y') . '/' . $fileName;
+                    if (!File::exists($uploadPath)) {
+                        File::makeDirectory($uploadPath, 0775, true, true);
+                    }
+                    $getFile->move($uploadPath, $fileName);
+                    $fileduthao->ten_file = $fileName;
+                    $fileduthao->duong_dan = $urlFile;
+                    $fileduthao->duoi_file = $extFile;
+                    $fileduthao->vb_du_thao_id = $duthao->id;
+                    $fileduthao->nguoi_tao = $this->user->id;
+                    $fileduthao->don_vi = $this->user->donvi_id;
+                    if ($extFile == 'pdf') {
+                        $fileduthao->stt = 1;
+                    } else {
+                        $fileduthao->stt = 3;
+                    }
+                    $fileduthao->save();
+                    if ($fileduthao->duoi_file == 'doc' || $fileduthao->duoi_file == 'docx') {
+                        if (config('system.convert_doc_to_pdf') == true) {
+                            $explodeFileDoc = explode('.', $fileduthao->ten_file);
+                            $filePdf = $explodeFileDoc[0] . '.' . 'pdf';
+                            $urlFile2 = '/vanBanDiFile_' . date('Y') . '/' . $filePdf;
+                            $convert = new OfficeConverter(public_path($fileduthao->duong_dan));
+                            $convert->convertTo($filePdf);
+                            $fileduthao2 = new Fileduthao();
+                            $fileduthao2->ten_file = $filePdf;
+                            $fileduthao2->duong_dan = $urlFile2;
+                            $fileduthao2->duoi_file = 'pdf';
+                            $fileduthao2->vb_du_thao_id = $fileduthao->vb_du_thao_id;
+                            $fileduthao2->nguoi_tao = $fileduthao->nguoi_tao;
+                            $fileduthao2->don_vi = $fileduthao->don_vi;
+                            $fileduthao2->stt = 1;
+                            $fileduthao2->save();
+                        }
+                    }
+
+                }
+
+            }
+            if ($filetrinhky && count($filetrinhky) > 0) {
+                foreach ($filetrinhky as $key => $getFile) {
+                    $typeArray = explode('.', $getFile->getClientOriginalName());
+                    $extFile = strtolower($typeArray[1]);
+                    $fileduthao = new Fileduthao();
+                    $fileName = date('Y_m_d') . '_' . Time() . '_' . $getFile->getClientOriginalName();
+
+                    $urlFile = '/vanBanDiFile_' . date('Y') . '/' . $fileName;
+                    if (!File::exists($uploadPath)) {
+                        File::makeDirectory($uploadPath, 0775, true, true);
+                    }
+                    $getFile->move($uploadPath, $fileName);
+                    $fileduthao->ten_file = $fileName;
+                    $fileduthao->duong_dan = $urlFile;
+                    $fileduthao->duoi_file = $extFile;
+                    $fileduthao->vb_du_thao_id = $duthao->id;
+                    $fileduthao->nguoi_tao = $this->user->id;
+                    $fileduthao->don_vi = $this->user->donvi_id;
+                    if ($extFile == 'pdf') {
+                        $fileduthao->stt = 2;
+                    } else {
+                        $fileduthao->stt = 3;
+                    }
+                    $fileduthao->save();
+                }
+                if ($fileduthao->duoi_file == 'doc' || $fileduthao->duoi_file == 'docx') {
+                    if (config('system.convert_doc_to_pdf') == true) {
+                        $explodeFileDoc = explode('.', $fileduthao->ten_file);
+                        $filePdf = $explodeFileDoc[0] . '.' . 'pdf';
+                        $urlFile2 = '/vanBanDiFile_' . date('Y') . '/' . $filePdf;
+                        $convert = new OfficeConverter(public_path($fileduthao->duong_dan));
+                        $convert->convertTo($filePdf);
+                        $fileduthao3 = new Fileduthao();
+                        $fileduthao3->ten_file = $filePdf;
+                        $fileduthao3->duong_dan = $urlFile2;
+                        $fileduthao3->duoi_file = 'pdf';
+                        $fileduthao3->vb_du_thao_id = $fileduthao->vb_du_thao_id;
+                        $fileduthao3->nguoi_tao = $fileduthao->nguoi_tao;
+                        $fileduthao3->don_vi = $fileduthao->don_vi;
+                        $fileduthao3->stt = 2;
+                        $fileduthao3->save();
+                    }
+                }
+
+            }
+            if ($lanhdaophong && count($lanhdaophong) > 0) {
+
+                if (array_diff($lanhdaophong, $idcanbophong) == null && count($idcanbophong) == count($lanhdaophong)) {
+                    //đây là trường hợp không thay đổi
+                }
+                if (array_diff($lanhdaophong, $idcanbophong) != null && count($idcanbophong) < count($lanhdaophong)) {
+                    //đây là trường hợp thêm n phần từ
+                    $hihi = array_diff($lanhdaophong, $idcanbophong);
+                    foreach ($hihi as $data) {
+                        $canbotontai = CanBoPhongDuThao:: where(['can_bo_id' => $data, 'du_thao_vb_id' => $id])->first();
+                        if ($canbotontai == null) {
+                            $canbophong = new CanBoPhongDuThao();
+                            $canbophong->can_bo_id = $data;
+                            $canbophong->du_thao_vb_id = $id;
+                            $canbophong->save();
+                        } else {
+                            $canbotontai->trang_thai = 1;
+                            $canbotontai->save();
+                        }
+                    }
+                    $xoaphantubochon = array_diff($idcanbophong, $lanhdaophong);
+                    foreach ($xoaphantubochon as $data) {
+                        $canbophong = CanBoPhongDuThao:: where(['can_bo_id' => $data, 'du_thao_vb_id' => $id])->first();
+                        $canbophong->trang_thai = 0;
+                        $canbophong->save();
+                    }
+                }
+                if (array_diff($lanhdaophong, $idcanbophong) == null && count($idcanbophong) > count($lanhdaophong)) {
+                    //đây là trường hợp xóa đi 1 phàn tử
+                    $xoa = array_diff($idcanbophong, $lanhdaophong);//xóa phần tử dc chọn
+
+                    foreach ($xoa as $data) {
+                        $canbophong = CanBoPhongDuThao:: where(['can_bo_id' => $data, 'du_thao_vb_id' => $id])->first();
+                        $canbophong->trang_thai = 0;
+                        $canbophong->save();
+                    }
+                }
+                if (array_diff($lanhdaophong, $idcanbophong) != null && count($idcanbophong) == count($lanhdaophong)) {
+                    //đây là trường hợp thay đổi người
+                    $thaydoicanbo = array_diff($lanhdaophong, $idcanbophong);
+                    foreach ($thaydoicanbo as $data) {
+                        $canbotontai = CanBoPhongDuThao:: where(['can_bo_id' => $data, 'du_thao_vb_id' => $id])->first();
+                        if ($canbotontai == null) {
+                            $canbophong = new CanBoPhongDuThao();
+                            $canbophong->can_bo_id = $data;
+                            $canbophong->du_thao_vb_id = $id;
+                            $canbophong->save();
+                        } else {
+                            $canbotontai->trang_thai = 1;
+                            $canbotontai->save();
+                        }
+                    }
+                    $Xoabochon = array_diff($idcanbophong, $lanhdaophong);
+                    foreach ($Xoabochon as $data) {
+                        $canbophong = CanBoPhongDuThao:: where(['can_bo_id' => $data, 'du_thao_vb_id' => $id])->first();
+                        $canbophong->trang_thai = 0;
+                        $canbophong->save();
+                    }
+                }
+
+
+            }
+            if ($lanhdaophongkhac && count($lanhdaophongkhac) > 0) {
+
+                if (array_diff($lanhdaophongkhac, $idcanbophongkhac) == null && count($idcanbophongkhac) == count($lanhdaophongkhac)) {
+                    //đây là trường hợp không thay đổi
+                }
+                if (array_diff($lanhdaophongkhac, $idcanbophongkhac) != null && count($idcanbophongkhac) < count($lanhdaophongkhac)) {
+                    //đây là trường hợp thêm n phần từ
+                    $hihikhac = array_diff($lanhdaophongkhac, $idcanbophongkhac);
+                    foreach ($hihikhac as $data) {
+                        $canbotontai = CanBoPhongDuThaoKhac:: where(['can_bo_id' => $data, 'du_thao_vb_id' => $id])->first();
+                        if ($canbotontai == null) {
+                            $canbophong = new CanBoPhongDuThaoKhac();
+                            $canbophong->can_bo_id = $data;
+                            $canbophong->du_thao_vb_id = $id;
+                            $canbophong->save();
+                        } else {
+                            $canbotontai->trang_thai = 1;
+                            $canbotontai->save();
+                        }
+                    }
+                    $xoaphantubochonkhac = array_diff($idcanbophongkhac, $lanhdaophongkhac);
+                    foreach ($xoaphantubochonkhac as $data) {
+                        $canbophong = CanBoPhongDuThaoKhac:: where(['can_bo_id' => $data, 'du_thao_vb_id' => $id])->first();
+                        $canbophong->trang_thai = 0;
+                        $canbophong->save();
+                    }
+                }
+                if (array_diff($lanhdaophongkhac, $idcanbophongkhac) == null && count($idcanbophongkhac) > count($lanhdaophongkhac)) {
+                    //đây là trường hợp xóa đi 1 phàn tử
+                    $xoakhac = array_diff($idcanbophongkhac, $lanhdaophongkhac);//xóa phần tử dc chọn
+
+                    foreach ($xoakhac as $data) {
+                        $canbophong = CanBoPhongDuThaoKhac:: where(['can_bo_id' => $data, 'du_thao_vb_id' => $id])->first();
+                        $canbophong->trang_thai = 0;
+                        $canbophong->save();
+                    }
+                }
+                if (array_diff($lanhdaophongkhac, $idcanbophongkhac) != null && count($idcanbophongkhac) == count($lanhdaophongkhac)) {
+                    //đây là trường hợp thay đổi người
+                    $thaydoicanbokhac = array_diff($lanhdaophongkhac, $idcanbophongkhac);
+                    foreach ($thaydoicanbokhac as $data) {
+                        $canbotontai = CanBoPhongDuThaoKhac:: where(['can_bo_id' => $data, 'du_thao_vb_id' => $id])->first();
+                        if ($canbotontai == null) {
+                            $canbophong = new CanBoPhongDuThao();
+                            $canbophong->can_bo_id = $data;
+                            $canbophong->du_thao_vb_id = $id;
+                            $canbophong->save();
+                        } else {
+                            $canbotontai->trang_thai = 1;
+                            $canbotontai->save();
+                        }
+                    }
+                    $Xoabochonkhac = array_diff($idcanbophongkhac, $lanhdaophongkhac);
+                    foreach ($Xoabochonkhac as $data) {
+                        $canbophong = CanBoPhongDuThaoKhac:: where(['can_bo_id' => $data, 'du_thao_vb_id' => $id])->first();
+                        $canbophong->trang_thai = 0;
+                        $canbophong->save();
+                    }
+                }
+
+
+            }
+            return redirect()->back()->with('success', 'cập nhật thành công !');
+
+        }
     }
 
     /**
@@ -278,6 +550,8 @@ class DuThaoVanBanController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $duthao = Duthaovanbandi::where('id',$id)->first();
+        $duthao->delete();
+        return redirect()->route('Danhsachduthao')->with('success','Xóa thành công !');
     }
 }
--- Modules/VanBanDi/Routes/web.php
+++ Modules/VanBanDi/Routes/web.php
@@ -18,10 +18,5 @@ Route::resource('du-thao-van-ban', 'DuThaoVanBanController');
 Route::get('Danh-sach-du-thao', array('as' => 'Danhsachduthao', 'uses' => 'DuThaoVanBanController@Danhsachduthao'));
 Route::get('thong-tin-du-thao-cu/{id}', array('as' => 'laythongtinduthaocu', 'uses' => 'DuThaoVanBanController@laythongtinduthaocu'));
 Route::get('thong-tin-du-thao-chot/{id}', array('as' => 'thongtinvanban', 'uses' => 'DuThaoVanBanController@thongtinvanban'));
-Route::match(['get', 'post'],
-    'thong-tin-du-thao-chot/{id}',
-    [
-        'as' =>'thongtinvanban' ,
-        'uses' => 'DuThaoVanBanController@thongtinvanban'
-    ]
-);
+Route::match(['get', 'post'], 'thong-tin-du-thao-chot/{id}', ['as' =>'thongtinvanban' , 'uses' => 'DuThaoVanBanController@thongtinvanban']);
+Route::get('du-thao-van-ban/delete/{id}', ['as' =>'duthaodelete' , 'uses' => 'DuThaoVanBanController@destroy']);
