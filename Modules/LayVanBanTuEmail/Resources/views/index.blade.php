440e9c98ee046222293fca5d8045bda4c6238297
Thanh <haithanhcx1@gmail.com>
2020-11-26 13:55:05 +0700

    06

---
+++ Modules/LayVanBanTuEmail/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layvanbantuemail::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('layvanbantuemail.name') !!}
+    </p>
+@endsection
