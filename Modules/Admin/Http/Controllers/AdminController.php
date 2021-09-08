8e2fc6ab54caa8b674b050410a6ff49eb9359443
Thanh <haithanhcx1@gmail.com>
2021-09-09 00:08:40 +0700

    vanban2

--- Modules/Admin/Http/Controllers/AdminController.php
+++ Modules/Admin/Http/Controllers/AdminController.php
@@ -163,9 +163,10 @@ class AdminController extends Controller
                 $vanBanDiChoSo = VanBanDi::
 //                where(['cho_cap_so' => 2])
                     where(function ($query) use ($lanhDaoSo){
-                        return  $query->where('phong_phat_hanh', $lanhDaoSo->don_vi_id)
-                            ->orWhere('truong_phong_ky', 2);
+                        return  $query->where('phong_phat_hanh', $lanhDaoSo->don_vi_id);
+//                            ->orWhere('truong_phong_ky', 2);
                     })
+                    ->where('truong_phong_ky', 2)
                     ->orderBy('created_at', 'desc')
                     ->count();
             } elseif (auth::user()->hasRole(VAN_THU_DON_VI)) {
